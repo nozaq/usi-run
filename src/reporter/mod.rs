3eb408ea65095b0b57ef9e8d7712933fafcaa9c0
Takashi Nozawa <nozaq@users.noreply.github.com>
2017-05-09 07:27:56 +0900

    Progress indicator enhancement (#4)
    * refactor: encapsulate logging modes into Reporter trait
    
    * feat: better progress indicator for simple reporter
    
    * feat: improve board reporter

---
+++ src/reporter/mod.rs
@@ -0,0 +1,23 @@
+use environment::Event;
+use shogi::Color;
+use stats::MatchStatistics;
+use shogi::usi::GuiCommand;
+use usi::EngineOutput;
+
+pub trait Reporter {
+    fn on_send_command(&mut self, Color, &GuiCommand, &str) {}
+    fn on_receive_command(&mut self, Color, &EngineOutput) {}
+    fn on_game_event(&mut self, &Event, &MatchStatistics) {}
+    fn on_match_finished(&mut self, stats: &MatchStatistics) {
+        println!("Total\tBlack\tWhite\tDraw");
+        println!("{}\t{}\t{}\t{}", stats.finished_games(), stats.black_wins(), stats.white_wins(), stats.draw_games());
+    }
+}
+
+mod board;
+mod command;
+mod simple;
+
+pub use self::board::*;
+pub use self::command::*;
+pub use self::simple::*;
---
+++ src/reporter/simple.rs
@@ -0,0 +1,60 @@
+use indicatif::{ProgressBar, ProgressDrawTarget, ProgressStyle};
+
+use environment::Event;
+use shogi::Color;
+use stats::MatchStatistics;
+
+use super::Reporter;
+
+#[derive(Default)]
+pub struct SimpleReporter {
+    current_bar: Option<ProgressBar>
+}
+
+impl Reporter for SimpleReporter {
+    fn on_game_event(&mut self, event: &Event, stats: &MatchStatistics) {
+        match *event {
+            Event::NewGame(_) => {
+                let current_game_num = stats.finished_games() + 1;
+                let num_games = stats.total_games();
+
+                let bar = ProgressBar::new_spinner();
+                bar.set_draw_target(ProgressDrawTarget::stderr());
+                bar.set_style(ProgressStyle::default_spinner()
+                    .tick_chars("|/-\\ ")
+                    .template("{prefix:.bold.dim} {spinner} {msg}"));
+                bar.set_prefix(&format!("[{}/{}]", current_game_num, num_games));
+                bar.set_message("Starting...");
+                self.current_bar = Some(bar);
+            },
+            Event::NewTurn(ref game) => {
+                if let Some(game) = game.read().ok() {
+                    match self.current_bar {
+                        Some(ref bar) => {
+                            bar.set_message(&format!("Move #{}", game.pos.ply()));
+                        },
+                        None => {}
+                    }
+                }
+            },
+            Event::GameOver(winner, reason) => {
+                match self.current_bar {
+                    Some(ref bar) => {
+                        let result = match winner {
+                            Some(c) => {
+                                let name = if c == Color::Black { "Black" } else { "White" };
+                                format!("{} won the game. ({:?})", name, reason)
+                            }
+                            None => {
+                                format!("Draw({:?})", reason)
+                            }
+                        };
+                        bar.finish_with_message(&result);
+                    },
+                    None => {}
+                }
+            }
+            _ => {}
+        }
+    }
+}
